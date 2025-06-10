5120a6cc8261ac8c6bbab5c2cfc009bb86805ca9
tjllis <floydjellis@gmail.com>
2025-06-11 01:38:01 +0200

    Fixed ft_printf
    Refactor: added ft_printf_utils.c

--- LIBFT_GNL_PRINTF/includes/ft_printf.h
+++ LIBFT_GNL_PRINTF/includes/ft_printf.h
@@ -4,20 +4,22 @@
 # include <stdarg.h>
 # include <stdlib.h>
 # include <limits.h>
+// # include "libft.h"
 
 # define NULL_PTR "(nil)"
 
 char	*ft_itoa_mod(int n);
-char	*ft_utoa(unsigned int n);
+int		ft_print_unsigned(unsigned int c);
 int		ft_printf(const char *format, ...);
 int		ft_printchar(int c);
+int		ft_printhex(unsigned long num, const char c);
 int		ft_printnum(int c);
+int		ft_printptr(void *ptr);
+int		ft_printpercent(void);
 int		ft_printstr(char *s);
 int		ft_print_unsigned(unsigned int c);
-int		ft_printptr(void *ptr);
-int		ft_printhex(unsigned long num, const char c);
-int		ft_printpercent(void);
 void	ft_to_hex(unsigned long num, char *s, int *i);
 void	ft_to_bighex(unsigned long num, char *s, int *i);
+char	*ft_utoa(unsigned int n);
 
 #endif
